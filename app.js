eeefd041e81fbb38511d1233814c5efce3a45a12
gchervet <gchervet@kennedy.edu.ar>
2017-11-30 11:25:01 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
+
+process.env.SECRET_KEY = "mybadasskey";
+
+var app = express();
+var secureRoutes = express.Router();
+
+//Controllers
+var authenticateController = require('./server/controllers/authenticate-controller');
+var dataController = require('./server/controllers/data-controller');
+
+//App uses
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use('/secure-api', secureRoutes);
+
+//Service Routes
+app.get('/api/authenticate', authenticateController.authenticate);
+
+    //Validation middleware
+    secureRoutes.use(function(req, res, correctCallbackFunction){
+        var token = req.body.token || req.headers['token'];
+    
+        if(token){
+            //res.send("we have a token");
+            jwt.verify(token, process.env.SECRET_KEY, function(err, decode){
+                if(err){
+                    res.status(500).send("Invalid token");
+                } else {
+                    correctCallbackFunction();
+                }
+            });
+        } else {
+            res.send("please send a token");
+        }
+
+    });
+
+secureRoutes.get('/get-data', dataController.getData);
+secureRoutes.post('/post-data', dataController.postData);
+
+app.listen(9000, function(){
+    console.log("server running on port 9000");
+})
---
+++ server/controllers/data-controller.js
@@ -0,0 +1,21 @@
+module.exports.getData = function(req, res){
+    
+    //if(err){
+    //    return res.status(500).send("Couldn't run the query");
+    //}
+
+    var people = {
+        firstName: 'Hola',
+        lastName: 'Chau'
+    };
+
+    res.json({data: people});
+}
+
+module.exports.postData = function(req, res){
+    
+    //if(err){
+    //    return res.status(500).send("Could not save the user");
+    //}
+    res.status(200).send("You have added a new person");
+}
